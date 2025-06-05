c42a641db3d3ec9131e30b716c0f16a9384191f9
Everaldo451 <everaldo.work.jr@gmail.com>
2025-06-05 05:00:30 -0300

    adiciona função de ler últimas n partidas e ordenar partidas em ordem decrescente (ultima partida realizada ate a primeira

--- backend/src/persistence/matches/read.py
+++ backend/src/persistence/matches/read.py
@@ -1,6 +1,13 @@
+from .order import order_matches_by_latest
 import csv
 
-def read_matches(matches_file_path: str):
+def read_all_matches(matches_file_path: str):
     with open(matches_file_path, newline="") as matches_data_file:
         match_reader = csv.reader(matches_data_file, delimiter=" ",quotechar="|")
         return [match_row for match_row in match_reader]
+    
+
+def read_latest_n_matches(matches_file_path:str, n:int):
+    all_matches = read_all_matches(matches_file_path)
+    return order_matches_by_latest(all_matches[-n:])
+    
