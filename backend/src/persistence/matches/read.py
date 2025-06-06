0b50ea6cb2daf08a53e7315ea24f338e68592081
Everaldo451 <everaldo.work.jr@gmail.com>
2025-06-06 16:37:02 -0300

    corrige a função de ler as ultimas n partidas

--- backend/src/persistence/matches/read.py
+++ backend/src/persistence/matches/read.py
@@ -9,5 +9,7 @@ def read_all_matches(matches_file_path: str):
 
 def read_latest_n_matches(matches_file_path:str, n:int):
     all_matches = read_all_matches(matches_file_path)
-    return order_matches_by_latest(all_matches[-n:])
+    latest_n_matches = all_matches[-n:]
+    order_matches_by_latest(latest_n_matches)
+    return latest_n_matches
     
