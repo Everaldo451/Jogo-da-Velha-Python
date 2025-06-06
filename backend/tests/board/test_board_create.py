0defa51646f5796d3f545965bdfc1e99a0d9a42e
Everaldo451 <everaldo.work.jr@gmail.com>
2025-06-06 18:59:32 -0300

    adiciona arquivos de teste do board

---
+++ backend/tests/board/test_board_create.py
@@ -0,0 +1,11 @@
+from src.board.create_board import create_board
+
+def test_create_board():
+    
+    board = create_board()
+
+    assert board == [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None],
+    ]
