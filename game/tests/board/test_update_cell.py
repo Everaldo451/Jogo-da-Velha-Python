9439108ed1143abccbcd631f4e1feee64e547fe3
Everaldo451 <everaldo.work.jr@gmail.com>
2025-06-09 20:30:42 -0300

    modifica teste de update da celula

--- game/tests/board/test_update_cell.py
+++ game/tests/board/test_update_cell.py
@@ -1,4 +1,5 @@
 from logic.board.update_cell import update_cell, is_cell_updatable
+from logic.board.board_helpers import calculate_cell_line, calculate_cell_column
 
 
 def test_is_cell_updatable():
@@ -8,9 +9,15 @@ def test_is_cell_updatable():
         ["X", None, None],
     ]
 
-    assert is_cell_updatable(board, 0)
-    assert not is_cell_updatable(board, 4)
-    assert not is_cell_updatable(board, 6)
+    line = calculate_cell_line(len(board), 0)
+    column = calculate_cell_column(len(board), 0)
+    assert is_cell_updatable(board, line, column)
+    line = calculate_cell_line(len(board), 4)
+    column = calculate_cell_column(len(board), 4)
+    assert not is_cell_updatable(board, line, column)
+    line = calculate_cell_line(len(board), 6)
+    column = calculate_cell_column(len(board), 6)
+    assert not is_cell_updatable(board, line, column)
 
 
 def test_update_cell():
@@ -19,9 +26,12 @@ def test_update_cell():
         [None, None, None],
         [None, None, None],
     ]
-
-    update_cell(board, 4, "X")
-    update_cell(board, 6, "O")
+    line = calculate_cell_line(len(board), 4)
+    column = calculate_cell_column(len(board), 4)
+    update_cell(board, line, column, "X")
+    line = calculate_cell_line(len(board), 6)
+    column = calculate_cell_column(len(board), 6)
+    update_cell(board, line, column, "O")
 
     assert board[1][1] == "X"
     assert board[2][0] == "O"
