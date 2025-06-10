5cf54391a01b8db2d1cbc006298552b676b5bb3e
Everaldo451 <everaldo.work.jr@gmail.com>
2025-06-10 07:38:08 -0300

    adicionar funções de mensagem novas e de mostrar tabuleiro

--- game/helpers/messagers.py
+++ game/helpers/messagers.py
@@ -5,3 +5,13 @@ def game_start():
 def game_end():
     #adicionar mensagem de fim de jogo.
     pass
+
+
+def victory_message():
+    #adicionar mensagem de vitoria
+    pass
+
+
+def empate_message():
+    #adicionar mensagem de empate
+    pass
